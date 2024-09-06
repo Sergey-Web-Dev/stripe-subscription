25152a77f9ffea941ddfd55263a4f2bdfa15e538
Sergey-Web-Dev <s.labodzin@gmail.com>
2024-09-06 16:40:12 +0300

    created finished back part

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +13,10 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors();
+
+  app.use('/stripe/webhook', express.raw({ type: 'application/json' }));
+
+  app.use(bodyParser.json());
 
   await app.listen(3000);
 }
