2ee2220669d9ca3788c280d8e3677e41ca4247f7
Sergey-Web-Dev <s.labodzin@gmail.com>
2024-09-20 10:08:30 +0300

    modified new version2 and finished front part

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StripeModule } from './modules/stripe/stripe.module';
 import { SubscriptionModule } from './modules/subscription/subscription.module';
-import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [StripeModule, SubscriptionModule, UserModule],
+  imports: [StripeModule, SubscriptionModule],
   controllers: [],
   providers: [],
 })
